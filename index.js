7343ea2fb91529c16b995611ceb3d8e23d502d68
Bartek Błęcki <bartek@blecki.pl>
2019-06-14 14:24:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var fs = require('fs');
+var colors = require('colors');
 
-fs.stat('./cat.jpg', function(err, stats) {
-    console.log(stats);
+fs.readdir('./', function (err, data) {
+    if (err) throw err;
+    console.log('Spis plików w katalogu:'.blue);
+    console.log(data);
+    fs.writeFile('./spis.txt', data, function (err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+    })
 });
